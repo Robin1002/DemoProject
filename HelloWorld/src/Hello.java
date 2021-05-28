e00eae94b5d7af0a89b93f8634bead72a02d03d7
Robin1002 <40015101+Robin1002@users.noreply.github.com>
2021-05-28 12:32:23 +0530

    Update Hello.java

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
       System.out.println("Hello world");
       System.out.println("Thanku");
+		System.out.println("End of Program");
 	}
 
 }
